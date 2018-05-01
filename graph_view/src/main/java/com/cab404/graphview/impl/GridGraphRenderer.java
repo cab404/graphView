580448797020443b7357a85799af383f379752f9
cab404 <me@cab404.ru>
2018-05-01 22:27:50 +0300

    moved labels down and animated them

--- graph_view/src/main/java/com/cab404/graphview/impl/GridGraphRenderer.java
+++ graph_view/src/main/java/com/cab404/graphview/impl/GridGraphRenderer.java
@@ -6,11 +6,15 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PointF;
 import android.graphics.RectF;
+import android.util.Log;
 
 import com.cab404.graphview.DataPoint;
 import com.cab404.graphview.GraphRenderer;
 import com.cab404.graphview.GraphView;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -22,6 +26,38 @@ public class GridGraphRenderer<A extends GridGraphRenderer.LabeledDataPoint & Da
     private Path path = new Path();
     private PointF tmp = new PointF();
     public Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    /**
+     * Minimum width between lines before labels will start disappearing
+     */
+    public int sectionMinimum = 32 * 2;
+
+    private HashMap<Float, Float> visibility = new HashMap<>();
+    private float visiblitiyDragUp = .15f;
+    private float visiblitiyDragDown = .1f;
+    private boolean changedFlag;
+
+    /**
+     * drags `what` to `to`, and sets `changedFlag` to true, if drag was performed
+     */
+    float drag(float what, float to) {
+        if (what == to) return to;
+        System.out.println("what " + what + " != to" + to );
+        changedFlag = true;
+        float dragged = what + (to - what) * (what > to ? visiblitiyDragDown : visiblitiyDragUp);
+        // If distance to final value is less than .01, we consider
+        // them practically same and engaging magnet clamps.
+        if (Math.abs(dragged - to) < .01f) dragged = to;
+        return dragged;
+    }
+
+    void dragPointVisibility(float value, float to) {
+        visibility.put(value, drag(visibility(value), to));
+    }
+
+    float visibility(float of){
+        if (!visibility.containsKey(of)) visibility.put(of, 0f);
+        return visibility.get(of);
+    }
 
     {
         paint.setStrokeWidth(1);
@@ -33,6 +69,7 @@ public class GridGraphRenderer<A extends GridGraphRenderer.LabeledDataPoint & Da
     public void render(GraphView view, Canvas canvas, RectF viewport, List<A> points) {
         path.reset();
 
+        paint.setAlpha(255);
         // drawing lines
         for (A point : points) {
             point.toWorld(tmp);
@@ -48,14 +85,46 @@ public class GridGraphRenderer<A extends GridGraphRenderer.LabeledDataPoint & Da
         paint.setStyle(Paint.Style.FILL_AND_STROKE);
         canvas.drawPath(path, paint);
 
+        int distanceSkipAccumulator = -sectionMinimum;
+
+        changedFlag = false;
         // and drawing titles
         for (A point : points) {
             point.toWorld(tmp);
+            float id = tmp.x;
             view.worldToCanvas(tmp);
+            distanceSkipAccumulator -= tmp.x;
 //            tmp.y *= -1;
-            canvas.drawText(point.getLabel(), tmp.x, tmp.y, paint);
+            if (-distanceSkipAccumulator >= sectionMinimum) {
+                dragPointVisibility(id, 1);
+                distanceSkipAccumulator = 0;
+            } else {
+                dragPointVisibility(id, 0f);
+            }
+
+            paint.setAlpha((int) (255 * visibility(id)));
+            canvas.drawText(point.getLabel(), tmp.x, canvas.getHeight(), paint);
+            // marking all on-screen coordinate visibilities with "+1", so we can detect and remove offscreen values
+            // (those always will be below 1.)
+            visibility.put(id, visibility.get(id) + 1);
+
+            distanceSkipAccumulator += tmp.x;
         }
 
+        // removing offscreen values
+        Iterator<Float> iterator = visibility.keySet().iterator();
+        while (iterator.hasNext())
+            if (visibility.get(iterator.next()) < 1f)
+                iterator.remove();
+        // removing marks
+        for (Float id : visibility.keySet())
+            visibility.put(id, visibility.get(id) - 1);
+
+
+        if (changedFlag)
+            view.invalidate();
+
+        changedFlag = false;
     }
 
     public interface LabeledDataPoint {
